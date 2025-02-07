3042e7e18a19e1307cadc131fa4fcddc3a930662
Mario Enrique  Vasquez Adriano <marioenriquevasquez4@gmail.com>
2025-02-07 13:11:27 -0800

    Implementacion de generacion de Sudoku y rompecabezas

--- src/lab3_abstract/sudokuSolucion.java
+++ src/lab3_abstract/sudokuSolucion.java
@@ -4,10 +4,41 @@
  */
 package lab3_abstract;
 
+import java.util.Random;
+
 /**
  *
  * @author chung
  */
-public class sudokuSolucion {
-    
+public class sudokuSolucion extends BaseSudoku{
+     public sudokuSolucion() {
+        tablerosudoku = new int[9][9];
+        GenerarSolucion();
+    }
+
+    @Override
+    public void GenerarSolucion() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                tablerosudoku[i][j] = (i * 3 + i / 3 + j) % 9 + 1;
+            }
+        }
+    }
+    @Override
+    public int[][] GenerarPuzzle() {
+        int[][] puzzle = new int[9][9];
+        Random rand = new Random();
+
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (rand.nextInt(3) == 0) {
+                    puzzle[i][j] =  tablerosudoku[i][j];
+                } else {
+                    puzzle[i][j] = 0;
+                }
+            }
+        }
+        return puzzle;
+    }
 }
