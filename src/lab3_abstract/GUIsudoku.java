cf8159ef6e8adb369fbd1296f31c45316b12d096
Mario Enrique  Vasquez Adriano <marioenriquevasquez4@gmail.com>
2025-02-07 13:45:43 -0800

    Se agrega en la clase GuiSudoku su funcionalidad

--- src/lab3_abstract/GUIsudoku.java
+++ src/lab3_abstract/GUIsudoku.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lab3_abstract;
+import javax.swing.JFrame;
 
 /**
  *
  * @author 50494
  */
-public class GUIsudoku {
-    
+public class GUIsudoku  extends JFrame{
+     private TableroSudoku tablero;
+
+    public GUIsudoku() {
+        setTitle("Sudoku");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+     
+        tablero = new TableroSudoku();
+        add(tablero.createBoardPanel());
+    }
 }
