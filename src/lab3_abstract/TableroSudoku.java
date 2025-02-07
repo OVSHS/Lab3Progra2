ca0b3b4f6b6a7caa8f080dc2dc85bf98f156ab76
Chung-Un <chungunyum23@gmail.com>
2025-02-07 15:23:28 -0600

    Tablero con algunas funciones

--- src/lab3_abstract/TableroSudoku.java
+++ src/lab3_abstract/TableroSudoku.java
@@ -4,10 +4,53 @@
  */
 package lab3_abstract;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 /**
  *
  * @author Mario
  */
 public class TableroSudoku {
-    
+    private JPanel panelTablero;
+    private JTextField[][] celdas;
+    private sudokuSolucion solucion;
+    private int[][] puzzle; 
+
+    public TableroSudoku() {
+        solucion = new sudokuSolucion();
+        puzzle = solucion.GenerarPuzzle();
+    }
+
+    public JPanel createBoardPanel() {
+        panelTablero = new JPanel(new GridLayout(9, 9));
+        celdas = new JTextField[9][9];
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                celdas[row][col] = new JTextField();
+                celdas[row][col].setHorizontalAlignment(JTextField.CENTER);
+
+                if (puzzle[row][col] != 0) {
+                    celdas[row][col].setText(String.valueOf(puzzle[row][col]));
+                    celdas[row][col].setEditable(false);
+                    celdas[row][col].setBackground(Color.LIGHT_GRAY);
+                } else {
+                    int r = row, c = col;
+                    celdas[row][col].addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            //falta funcion
+                        }
+                    });
+                }
+                panelTablero.add(celdas[row][col]);
+            }
+        }
+        return panelTablero;
+    }
+
 }
